b58b08bc01d01b37439dce68db9b0406281cdd74
biogit <5da8b965@opayq.com>
2019-08-20 22:14:58 +0100

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -13,7 +13,8 @@ class NotFoundPage extends React.Component {
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="404: Not Found" />
         <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <p>You just left the *Extraterrestrial Highway* for a route that doesn&#39;t exist.</p>
+        ![Dead End Sign](./dead-end.jpg)
       </Layout>
     )
   }
